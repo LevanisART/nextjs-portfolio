957a5ff080aad22f7bc6071d87c12f5281d1c02f
Levani Qotolashvili <levani.qotolashvili@gmail.com>
2019-06-28 20:27:17 +0400

    Fixed about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,10 +5,10 @@ class About extends React.Component {
     return (
       <div className="about animatedParent">
         <div className="container d-flex justify-content-between">
-          <div className="col-12 col-md-7 pl-0 pr-4 about-left animated fadeInLeft">
+          <div className="col-12 col-md-7 pl-0 pr-0 pr-md-4 about-left animated fadeInLeft go">
             <div className="info mt-4">
               <h1>Freelance Web Designer & Developer based in Georgia</h1>
-              <p className="mt-3 pr-5 mr-4">I'm a Web Designer and Developer with more than 2 years of freelance experience. Each of the websites I design and build are based on human-centered experience. I am always looking for the newest and best way to solve all of my clients problems. If that's what you're looking for, I am ready to support your project using my knoweledge and experience.</p>
+              <p className="mt-3 pr-0 mr-0 pr-md-5 mr-md-4">I'm a Web Designer and Developer with more than 2 years of freelance experience. Each of the websites I design and build are based on human-centered experience. I am always looking for the newest and best way to solve all of my clients problems. If that's what you're looking for, I am ready to support your project using my knoweledge and experience.</p>
             </div>
           </div>
           <div className="col-md-5 about-right animated fadeInRight">
