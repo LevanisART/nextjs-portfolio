469b7a32f7c0c1bc0278e528476b0e8d30058901
Levani Qotolashvili <levani.qotolashvili@gmail.com>
2019-11-10 23:12:53 +0400

    syntax correction

--- portfolio.config.js
+++ portfolio.config.js
@@ -4,7 +4,7 @@ module.exports = {
     jobtitle: "UI/UX Designer & Front-end Developer",
     author: 'Levani',
     image: '/static/images/levanisart.png',
-    description: "Levani's ART Portfolio",
+    description: "Levani Q. Portfolio",
     keywords: 'ui, ux, designer, portfolio, web developer portfolio, front end developer portfolio, resume, ui/ux designer, ui ux portfolio, app designer',
     siteUrl: 'https://levanisart.com',
     projectsPerPage: 20
