94acb48e51772c01cbc2aaa84544c27e3a78c4bc
Levani's ART <levani.qotolashvili@gmail.com>
2019-12-22 17:37:39 +0400

    Changed color contrast

--- portfolio.config.js
+++ portfolio.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   siteMeta: {
-    name: "Levani Q.",
+    name: "Levan K.",
     jobtitle: "UI/UX Designer & Front-end Developer",
     author: 'Levani',
     image: '/static/images/levanisart.png',
-    description: "Levani Q. Portfolio",
+    description: "Levani K. Portfolio",
     keywords: 'ui, ux, designer, portfolio, web developer portfolio, front end developer portfolio, resume, ui/ux designer, ui ux portfolio, app designer',
     siteUrl: 'https://levanisart.com',
     projectsPerPage: 20
